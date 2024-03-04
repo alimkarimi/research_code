94cdd0cf723b7ef78839306b9aaaf7d3377e4730
Alim K <alimisakarimi@gmail.com>
2024-03-03 21:46:46 -0500

    training update - use GPU

--- models/train_ae.py
+++ models/train_ae.py
@@ -4,7 +4,7 @@ from torch import optim
 
 import sys
 sys.path.append('..')
-from dataloading_scripts.feature_dataloader import FeaturesDataset
+from dataloading_scripts.hyperspectral_dataloader import FeaturesDataset
 from models import HyperspectralAE
 from hyperspectral_lidar_processing.hyperspectral_plot_extraction import get_visual_hyperspectral_rgb
 
@@ -28,11 +28,40 @@ hyp_np = np.load(path + '/' + files[0])
 print(hyp_np.max(), hyp_np.min())
 print(hyp_np.shape)
 
-
-
 in_channels = 136
-height = 136
-width = 20
+height = 130
+width = 42
 
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print(device)
+else:
+    device = torch.device("cpu")
+    print(device)
+
+# init model:
 ae_model = HyperspectralAE(in_channels, height, width)
-ae_model(torch.tensor(hyp_np).float())
+ae_model = ae_model.to(device)
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+
+training_data = FeaturesDataset(field = 'hips_2021', train=True, test=False, load_individual=True, load_series = False, debug=False)
+training_dataloader = torch.utils.data.DataLoader(training_data, batch_size=1, num_workers = 0, drop_last=False)
+
+for n, batch_data in enumerate(training_dataloader):
+    # unpack:
+    img, GT = batch_data
+    img = img.to(torch.float32)
+    print(img.dtype)
+    print(img.shape)
+    img = img.to(device)
+    img = torch.squeeze(img, 0)
+    print(img.shape)
+
+    out = ae_model(img)
+    break
+
+
+
+#ae_model(torch.tensor(hyp_np).float
