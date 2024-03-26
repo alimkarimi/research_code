96966c02070d70726720059013aa807bae047999
Alim K <alimisakarimi@gmail.com>
2024-03-25 21:34:06 -0400

    mulitmodal support

--- models/train_multimodal_lstm.py
+++ models/train_multimodal_lstm.py
@@ -42,7 +42,7 @@ lidar_state_dict = torch.load(lidar_ae_model_path)
 
 # init RNN:
 rnn = RNN(batch_size = 1, concat_based_LSTM = True, addition_based_LSTM = False,
-           hidden_size = 534, cell_size = 534) # lstm gets instantiated inside RNN class.
+           hidden_size = 100, cell_size = 100, input_size=534, debug=True) # lstm gets instantiated inside RNN class.
 rnn = rnn.to(torch.float32)
 
 # instantiate optimizer
@@ -58,11 +58,13 @@ if torch.backends.mps.is_available():
     device = torch.device("mps")
     lidar_ae_model = lidar_ae_model.to(device)
     hyp_ae_model = hyp_ae_model.to(device)
+    rnn = rnn.to(device)
     print('USING DEVICE:', device, '(GPU)')
 else:
     device = torch.device("cpu")
     lidar_ae_model = lidar_ae_model.to(device)
     hyp_ae_model = hyp_ae_model.to(device)
+    rnn = rnn.to(device)
     print('USING DEVICE:', device, '(CPU)')
 
 if cpu_override:
@@ -70,6 +72,8 @@ if cpu_override:
     device = torch.device("cpu")
     lidar_ae_model = lidar_ae_model.to("cpu")
     hyp_ae_model = hyp_ae_model.to("cpu")
+    rnn = rnn.to("cpu")
+
 
 
 # then, we need to concatenate the latent representations of those vectors with the weather observations
@@ -77,36 +81,80 @@ if cpu_override:
 training_data = FeaturesDataset(field = 'hips_2021', train=True, test=False, load_individual=False, load_series = True, debug=False)
 training_dataloader = torch.utils.data.DataLoader(training_data, batch_size=1, num_workers = 0, drop_last=False)
 
-for n, batch in enumerate(training_dataloader):
-    # first, unpack batch:
-    img, GT, point_cloud, GDD, PREC = batch # point_cloud is a list of tensors b/c of their variable size.
-    
+running_loss = []
+epochs = 1
+#init loss function:
+criteron = nn.MSELoss()
+total_loss = 0
 
-    # concat GDD and PREC into weather tensor:
-    weather = torch.concat([GDD, PREC]).to(torch.float32)
-    weather = weather.to(device)
-    print(weather.shape) # 2 x timepoints
+for epoch in range(epochs):
+    for n, batch in enumerate(training_dataloader):
+        # first, unpack batch:
+        img, GT, point_cloud, GDD, PREC = batch # point_cloud is a list of tensors b/c of their variable size.
+        print(img.shape, GT.shape, len(point_cloud), GDD.shape, PREC.shape)
+        T = img.shape[1]
+        print(T, 'this is T')
+        # create empty tensor to hold the concatenated embeddings:
+        full_feature_vector = torch.zeros((T, 534))
 
-    # move hyperspectral and lidar data to float32 precision:
-    img = img.squeeze(0)
-    img = img.to(torch.float32)
-    img = img.to(device)
-    print(img.shape)
-    # point_cloud = point_cloud.to(torch.float32)
-    # point_cloud = point_cloud.to(device)
+        # concat GDD and PREC into weather tensor:
+        weather = torch.concat([GDD, PREC]).to(torch.float32)
+        weather = weather.to(device)
+        print(weather.shape) # 2 x timepoints
 
-    # run img through hyperspectral AE, run point_cloud through lidar AE:
+        # move hyperspectral and lidar to float32 precision:
+        img = img.squeeze(0)
+        img = img.to(torch.float32)
+        img = img.to(device)
+        for t in range(len(point_cloud)):
+            point_cloud[t] = point_cloud[t].to(torch.float32)
+            point_cloud[t] = point_cloud[t].to(device)
 
-    hyp_embedding = hyp_ae_model(img) # hyp_embdding is 500 dims
-    print('ran this')
-    lidar_embedding = lidar_ae_model(point_cloud) # lidar_embedding is 1 x 32 x 1
-    lidar_embedding = torch.squeeze(lidar_embedding, 0).squeeze(1) # squeeze from 1 x 32 x 1 to 32
+        # put each hyperspectral image from the timeseries through autoencoder:
+        print(img.shape)
+        # point_cloud = point_cloud.to(torch.float32)
+        # point_cloud = point_cloud.to(device)
 
-    # concatenate full feature vector
-    full_feature_vector = torch.concat([hyp_embedding, lidar_embedding, weather]) # 500 + 32 + 2 = 534 dimensions
+        # run img through hyperspectral AE, run point_cloud through lidar AE:
+        print('before hyp embedding')
+        for t in range(img.shape[0]):
+            hyp_embedding = hyp_ae_model(img[t]) # hyp_embdding is 500 dims
+            full_feature_vector[t, 0:500] = hyp_embedding
+        print('ran this')
 
-    rnn_out = rnn(full_feature_vector)
-    
+        for t in range(T):
+            print(point_cloud[t].shape)
+            #temp_pc = torch.squeeze(point_cloud[t], 0)
+            lidar_embedding = lidar_ae_model(point_cloud[t]) # lidar_embedding is 1 x 32 x 1
+            lidar_embedding = torch.squeeze(lidar_embedding, 0)
+            print(lidar_embedding.shape)
+            lidar_embedding = torch.squeeze(lidar_embedding, 1)
+            full_feature_vector[t, 500: 532] = lidar_embedding
 
+        # append the weather data into full feature vector:
+        weather = weather.T # reshape weather data to T x data
+        for t in range(T):
+            full_feature_vector[t, 532:] = weather[t]
 
+        full_feature_vector = full_feature_vector.to(device)
+
+        rnn_out = rnn(full_feature_vector)
+
+        ct, ht, pred, predictions_in_series = rnn_out
+        print(GT.shape, predictions_in_series.shape)
+
+        loss = criteron(predictions_in_series, GT.to(torch.float32).to(device).T)
+        print(loss) 
+
+        loss.backward()
+
+        optimizer.step()
+
+        total_loss += loss.item()
+
+        if (n + 1) % 50 == 0:
+            avg_loss = total_loss / 50
+            running_loss.append(avg_loss)
+            print("Loss in epoch", epoch, " is ", avg_loss)
+            total_loss = 0
 
